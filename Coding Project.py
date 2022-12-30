705e84ce2b5aaeb9d6b503239ad988436c070d1a
adheeb4 <117083156+adheeb4@users.noreply.github.com>
2022-12-30 00:24:47 +0000

    Update Coding Project.py

--- Coding Project.py
+++ Coding Project.py
@@ -14,3 +14,7 @@ rainfall = np.array(array[:, 0]).reshape((-1, 1))
 productivity = np.array(array[:, 1])
 plt.scatter(rainfall, productivity)
 model = LinearRegression().fit(rainfall, productivity)
+slope = model.coef_
+intercept = model.intercept_
+plt.plot(rainfall, slope*rainfall+intercept, color='black')
+prediction = model.predict([[245]])
